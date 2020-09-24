eb6cf9a2ef0d3345679fed0c48aca9d5c02436bb
Joshua-Micheletti <joshua.e.n.micheletti@gmail.com>
2020-09-24 16:13:10 +0200

    added automatic input switching and removed case sensitive inputs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # PlanetZooGenesCalculator
 A tool for calculating genes obtained through breeding in Planet Zoo
 
-Electron Version: https://drive.google.com/file/d/1pssEtv-oKGPR_Pr5YklyfeBMYPrnFvmK/view?usp=sharing
+Electron Version: https://drive.google.com/file/d/1eRoLtR7Fe5c8BLqmnlaLZOpZqjzdHUGc/view?usp=sharing
--- scripts/inputs.js
+++ scripts/inputs.js
@@ -1,5 +1,23 @@
 doneTypingInterval = 2000
 
+var change = false;
+$(".inputs").keyup(function () {
+
+  if (this.value.length < 2) {
+    change = true;
+  }
+
+  if (this.value.length > 2) {
+    change = false;
+  }
+
+  if (this.value.length == 2 && change) {
+    $(".inputs").eq( $(".inputs").index(this) + 1).focus();
+    change = false;
+  }
+});
+
+
 // FATHER FERTILITY INPUT
 
 var timerGene1Ff;               //timer identifier
@@ -19,6 +37,11 @@ $('body').on('keydown', '#gene1Ff', function () {
 //user is "finished typing," do something
 function saveGene1Ff () {
   fatherGenesf[0] = $('#gene1Ff').val();
+
+  for (var i = 0; i < fatherGenesf[0].length; i++) {
+    fatherGenesf[0] = fatherGenesf[0].replace(fatherGenesf[0][i], fatherGenesf[0].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -40,6 +63,11 @@ $('body').on('keydown', '#gene2Ff', function () {
 //user is "finished typing," do something
 function saveGene2Ff () {
   fatherGenesf[1] = $('#gene2Ff').val();
+
+  for (var i = 0; i < fatherGenesf[1].length; i++) {
+    fatherGenesf[1] = fatherGenesf[1].replace(fatherGenesf[1][i], fatherGenesf[1].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -61,6 +89,11 @@ $('body').on('keydown', '#gene3Ff', function () {
 //user is "finished typing," do something
 function saveGene3Ff () {
   fatherGenesf[2] = $('#gene3Ff').val();
+
+  for (var i = 0; i < fatherGenesf[2].length; i++) {
+    fatherGenesf[2] = fatherGenesf[2].replace(fatherGenesf[2][i], fatherGenesf[2].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -82,6 +115,11 @@ $('body').on('keydown', '#gene4Ff', function () {
 //user is "finished typing," do something
 function saveGene4Ff () {
   fatherGenesf[3] = $('#gene4Ff').val();
+
+  for (var i = 0; i < fatherGenesf[3].length; i++) {
+    fatherGenesf[3] = fatherGenesf[3].replace(fatherGenesf[3][i], fatherGenesf[3].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -103,6 +141,11 @@ $('body').on('keydown', '#gene5Ff', function () {
 //user is "finished typing," do something
 function saveGene5Ff () {
   fatherGenesf[4] = $('#gene5Ff').val();
+
+  for (var i = 0; i < fatherGenesf[4].length; i++) {
+    fatherGenesf[4] = fatherGenesf[4].replace(fatherGenesf[4][i], fatherGenesf[4].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -124,6 +167,11 @@ $('body').on('keydown', '#gene6Ff', function () {
 //user is "finished typing," do something
 function saveGene6Ff () {
   fatherGenesf[5] = $('#gene6Ff').val();
+
+  for (var i = 0; i < fatherGenesf[5].length; i++) {
+    fatherGenesf[5] = fatherGenesf[5].replace(fatherGenesf[5][i], fatherGenesf[5].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -147,6 +195,11 @@ $('body').on('keydown', '#gene1Fi', function () {
 //user is "finished typing," do something
 function saveGene1Fi () {
   fatherGenesi[0] = $('#gene1Fi').val();
+
+  for (var i = 0; i < fatherGenesi[0].length; i++) {
+    fatherGenesi[0] = fatherGenesi[0].replace(fatherGenesi[0][i], fatherGenesi[0].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -168,6 +221,11 @@ $('body').on('keydown', '#gene2Fi', function () {
 //user is "finished typing," do something
 function saveGene2Fi () {
   fatherGenesi[1] = $('#gene2Fi').val();
+
+  for (var i = 0; i < fatherGenesi[1].length; i++) {
+    fatherGenesi[1] = fatherGenesi[1].replace(fatherGenesi[1][i], fatherGenesi[1].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -189,6 +247,11 @@ $('body').on('keydown', '#gene3Fi', function () {
 //user is "iinished typing," do something
 function saveGene3Fi () {
   fatherGenesi[2] = $('#gene3Fi').val();
+
+  for (var i = 0; i < fatherGenesi[2].length; i++) {
+    fatherGenesi[2] = fatherGenesi[2].replace(fatherGenesi[2][i], fatherGenesi[2].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -210,6 +273,11 @@ $('body').on('keydown', '#gene4Fi', function () {
 //user is "finished typing," do something
 function saveGene4Fi () {
   fatherGenesi[3] = $('#gene4Fi').val();
+
+  for (var i = 0; i < fatherGenesi[3].length; i++) {
+    fatherGenesi[3] = fatherGenesi[3].replace(fatherGenesi[3][i], fatherGenesi[3].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -231,6 +299,11 @@ $('body').on('keydown', '#gene5Fi', function () {
 //user is "iinished typing," do something
 function saveGene5Fi () {
   fatherGenesi[4] = $('#gene5Fi').val();
+
+  for (var i = 0; i < fatherGenesi[4].length; i++) {
+    fatherGenesi[4] = fatherGenesi[4].replace(fatherGenesi[4][i], fatherGenesi[4].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -252,6 +325,11 @@ $('body').on('keydown', '#gene6Fi', function () {
 //user is "finished typing," do something
 function saveGene6Fi () {
   fatherGenesi[5] = $('#gene6Fi').val();
+
+  for (var i = 0; i < fatherGenesi[5].length; i++) {
+    fatherGenesi[5] = fatherGenesi[5].replace(fatherGenesi[5][i], fatherGenesi[5].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -276,6 +354,11 @@ $('body').on('keydown', '#gene1Mf', function () {
 //user is "finished typing," do something
 function saveGene1Mf () {
   motherGenesf[0] = $('#gene1Mf').val();
+
+  for (var i = 0; i < motherGenesf[0].length; i++) {
+    motherGenesf[0] = motherGenesf[0].replace(motherGenesf[0][i], motherGenesf[0].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -297,6 +380,11 @@ $('body').on('keydown', '#gene2Mf', function () {
 //user is "finished typing," do something
 function saveGene2Mf () {
   motherGenesf[1] = $('#gene2Mf').val();
+
+  for (var i = 0; i < motherGenesf[1].length; i++) {
+    motherGenesf[1] = motherGenesf[1].replace(motherGenesf[1][i], motherGenesf[1].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -318,6 +406,11 @@ $('body').on('keydown', '#gene3Mf', function () {
 //user is "finished typing," do something
 function saveGene3Mf () {
   motherGenesf[2] = $('#gene3Mf').val();
+
+  for (var i = 0; i < motherGenesf[2].length; i++) {
+    motherGenesf[2] = motherGenesf[2].replace(motherGenesf[2][i], motherGenesf[2].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -339,6 +432,11 @@ $('body').on('keydown', '#gene4Mf', function () {
 //user is "finished typing," do something
 function saveGene4Mf () {
   motherGenesf[3] = $('#gene4Mf').val();
+
+  for (var i = 0; i < motherGenesf[3].length; i++) {
+    motherGenesf[3] = motherGenesf[3].replace(motherGenesf[3][i], motherGenesf[3].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -360,6 +458,11 @@ $('body').on('keydown', '#gene5Mf', function () {
 //user is "finished typing," do something
 function saveGene5Mf () {
   motherGenesf[4] = $('#gene5Mf').val();
+
+  for (var i = 0; i < motherGenesf[4].length; i++) {
+    motherGenesf[4] = motherGenesf[4].replace(motherGenesf[4][i], motherGenesf[4].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -381,6 +484,11 @@ $('body').on('keydown', '#gene6Mf', function () {
 //user is "finished typing," do something
 function saveGene6Mf () {
   motherGenesf[5] = $('#gene6Mf').val();
+
+  for (var i = 0; i < motherGenesf[5].length; i++) {
+    motherGenesf[5] = motherGenesf[5].replace(motherGenesf[5][i], motherGenesf[5].charAt(i).toUpperCase());
+  }
+
   checkReadyf();
 }
 
@@ -404,6 +512,11 @@ $('body').on('keydown', '#gene1Mi', function () {
 //user is "finished typing," do something
 function saveGene1Mi () {
   motherGenesi[0] = $('#gene1Mi').val();
+
+  for (var i = 0; i < motherGenesi[0].length; i++) {
+    motherGenesi[0] = motherGenesi[0].replace(motherGenesi[0][i], motherGenesi[0].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -425,6 +538,11 @@ $('body').on('keydown', '#gene2Mi', function () {
 //user is "finished typing," do something
 function saveGene2Mi () {
   motherGenesi[1] = $('#gene2Mi').val();
+
+  for (var i = 0; i < motherGenesi[1].length; i++) {
+    motherGenesi[1] = motherGenesi[1].replace(motherGenesi[1][i], motherGenesi[1].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -446,6 +564,11 @@ $('body').on('keydown', '#gene3Mi', function () {
 //user is "iinished typing," do something
 function saveGene3Mi () {
   motherGenesi[2] = $('#gene3Mi').val();
+
+  for (var i = 0; i < motherGenesi[2].length; i++) {
+    motherGenesi[2] = motherGenesi[2].replace(motherGenesi[2][i], motherGenesi[2].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -467,6 +590,11 @@ $('body').on('keydown', '#gene4Mi', function () {
 //user is "finished typing," do something
 function saveGene4Mi () {
   motherGenesi[3] = $('#gene4Mi').val();
+
+  for (var i = 0; i < motherGenesi[3].length; i++) {
+    motherGenesi[3] = motherGenesi[3].replace(motherGenesi[3][i], motherGenesi[3].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -488,6 +616,11 @@ $('body').on('keydown', '#gene5Mi', function () {
 //user is "iinished typing," do something
 function saveGene5Mi () {
   motherGenesi[4] = $('#gene5Mi').val();
+
+  for (var i = 0; i < motherGenesi[4].length; i++) {
+    motherGenesi[4] = motherGenesi[4].replace(motherGenesi[4][i], motherGenesi[4].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
@@ -509,6 +642,11 @@ $('body').on('keydown', '#gene6Mi', function () {
 //user is "finished typing," do something
 function saveGene6Mi () {
   motherGenesi[5] = $('#gene6Mi').val();
+
+  for (var i = 0; i < motherGenesi[5].length; i++) {
+    motherGenesi[5] = motherGenesi[5].replace(motherGenesi[5][i], motherGenesi[5].charAt(i).toUpperCase());
+  }
+
   checkReadyi();
 }
 
